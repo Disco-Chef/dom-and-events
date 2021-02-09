7126c32e65d59657bd295ca6e7395d7cd2babafa
Nicolas Agathos <nicolas.agathos@gmail.com>
2021-02-09 10:35:52 +0100

    lecture batch 535

---
+++ src/index.js
@@ -0,0 +1,60 @@
+// const list = document.querySelector("#players");
+// console.log(list)
+
+// list.insertAdjacentHTML("beforeend", "<li>Luke</li>");
+// list.insertAdjacentHTML("beforeend", "<li>Anakin</li>");
+
+// const list = document.querySelector("#players");
+// const element = list.querySelector(".red");
+// console.log(element.innerText);
+
+// const countries = document.querySelector("#fifa-wins li");
+// const items = document.querySelectorAll("#fifa-wins li");
+// console.log(items)
+
+// items.forEach((item) => {
+//   console.log(item.innerText);
+// });
+
+// const list = document.querySelector('#fifa-wins');
+// list.insertAdjacentHTML('beforeend', '<li>France (2 wins)</li>');
+
+// // Hide
+// element.style.display = "none";
+
+// // Show
+// element.style.display = "";
+
+// element.classList.add("red");
+// element.classList.remove("red");
+// element.classList.toggle("red");
+
+// const emailInput = document.getElementById("email");
+// // Read
+// console.log(emailInput.value);
+// // Write
+// emailInput.value = "john@gmail.com";
+
+// const home = document.getElementById("home");
+// console.log(home.innerText);
+// console.log(home.innerHTML);
+// console.log(home.attributes.href.value);
+
+// home.innerHTML = "Le Wagon <strong>rocks</strong>!";
+
+// const boris = document.getElement
+// console.log(boris.dataset.uid);
+// console.log(boris.dataset.githubNickname);
+
+// const romain = document.getElementById("romain");
+// romain.addEventListener("click", (event) => {
+//   console.log(event);
+//   console.log(event.currentTarget);
+// });
+
+
+// document.querySelectorAll("img").forEach((img) => {
+//   img.addEventListener("click", (event) => {
+//     event.currentTarget.classList.toggle("img-circle");
+//   });
+// });
